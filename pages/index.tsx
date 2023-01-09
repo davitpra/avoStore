b68db58715d93ca1ff292adb8130b4f43b699ce9
David <j.david.prado1716@gmail.com>
2023-01-09 13:00:46 -0500

    Server Side Rendering: getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+export const getServerSideProps = async () => {
+  // la función fetch debe de venir desde una libreria que nos ayude con la tarea
+	const response = await fetch('https://avo-store-8jq9.vercel.app/api/avo')
+  console.log(response)
+  const { data }: TAPIAvoResponse = await response.json()
+	// Devuelve un objecto el cual luego se pasara como prop
+	// en el componente
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
+  return {
+    props: {
+      productList: data,
+    },
+  }
+}
 
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
